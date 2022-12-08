d455b47f5822222cca9223d0651be8e25d5c7260
Pouria Asrzad <poriaasrzad@gmail.com>
2022-12-08 14:17:06 +0330

    installreact

---
+++ todo/src/components/Form/Form.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <div>
+      <form>
+        <label htmlFor="title">title</label>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
